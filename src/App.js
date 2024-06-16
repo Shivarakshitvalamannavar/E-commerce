f4920175a8ec46e05fc4a7a14e05c88749e8cd70
codeboi234 <140171360+codeboi234@users.noreply.github.com>
2024-06-16 18:50:34 +0530

    Updating Our Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './categories.styles.scss'
 
-function App() {
+import Categories from './components/database/database.component';
+
+
+const App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Categories/>
   );
-}
+};
 
 export default App;
